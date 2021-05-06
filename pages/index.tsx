dde58794d2aa468808611da98b62527da2e8c1c5
Zack Davis <zack.davis88@hotmail.com>
2021-05-06 15:48:42 -0700

    basic hello-world nextjs app

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import { GetServerSideProps } from 'next'
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  return {
+    props: {}
+  };
+};
+
+export default function Home(props) {
+  return (
+    <div>
+      Hello, World!
+    </div>
+  )
+};
