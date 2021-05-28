690b2ec91d9f861f3d5ccb840eac82b98448a7ff
Zack Davis <zack.davis88@hotmail.com>
2021-05-28 16:30:26 -0700

    adding palette colors for error

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,7 @@
 import {createMuiTheme} from "@material-ui/core/styles";
 import blue from "@material-ui/core/colors/blue";
 import grey from "@material-ui/core/colors/grey";
+import red from "@material-ui/core/colors/red";
 
 const theme = createMuiTheme({
   breakpoints: {
@@ -23,7 +24,11 @@ const theme = createMuiTheme({
       main: grey["400"],
       dark: grey["500"]
     },
-
+    error: {
+      light: red["700"],
+      main: red["800"],
+      dark: red["900"]
+    }
   }
 });
 
