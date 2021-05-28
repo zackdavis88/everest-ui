f25bbadbc98bad177fb86ef45fd96e32dafec78f
Zack Davis <zack.davis88@hotmail.com>
2021-05-28 15:19:55 -0700

    updating authenticate dispatcher to work with updated api middleware

--- src/store/actions/auth.ts
+++ src/store/actions/auth.ts
@@ -1,17 +1,24 @@
-import request from "superagent";
 import {Buffer} from "buffer";
 
 export const TOKEN_REQUEST = "TOKEN_REQUEST";
 export const TOKEN_FAILURE = "TOKEN_FAILURE";
 export const TOKEN_SUCCESS = "TOKEN_SUCCESS";
-export const LOGOUT = "LOGOUT";
 
 // Get Auth Token
 export const authenticate = (username: string, password: string) => dispatch => {
   return dispatch({
     types: [TOKEN_REQUEST, TOKEN_SUCCESS, TOKEN_FAILURE],
-    request: request
-              .get("/api/auth")
-              .set("x-everest-basic", `Basic ${Buffer.from(`${username}:${password}`).toString("base64")}`)
+    reqUrl: "/api/auth",
+    reqOptions: {
+      headers: {
+        "x-everest-basic": `Basic ${Buffer.from(`${username}:${password}`).toString("base64")}`
+      }
+    }
+  });
+};
+
+export const logout = () => dispatch => {
+  return dispatch({
+    type: TOKEN_FAILURE, // TOKEN_FAILURE wipes the state back to initial values, which should be the exact logic I need...
   });
 };
