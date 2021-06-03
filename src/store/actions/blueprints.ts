f7df39fbfa0ca5df6b3a3a390d591b56ab156eb6
Zack Davis <zack.davis88@hotmail.com>
2021-06-03 16:31:16 -0700

    create redux action for reseting blueprints redux state

--- src/store/actions/blueprints.ts
+++ src/store/actions/blueprints.ts
@@ -2,3 +2,5 @@ export const BLUEPRINT_REQUEST = "BLUEPRINT_REQUEST";
 export const BLUEPRINT_FAILURE = "BLUEPRINT_FAILURE";
 export const BLUEPRINT_SUCCESS = "BLUEPRINT_SUCCESS";
 export const RESET_BLUEPRINTS = "RESET_BLUEPRINTS";
+
+export const resetBlueprints = () => dispatch => dispatch({ type: RESET_BLUEPRINTS });
