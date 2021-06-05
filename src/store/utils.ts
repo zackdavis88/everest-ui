1b6c013b94d15d8b0f0e7c8c218b176eeb99d34a
Zack Davis <zack.davis88@hotmail.com>
2021-06-05 08:14:47 -0700

    adding helper method to format a url with query string

--- src/store/utils.ts
+++ src/store/utils.ts
@@ -57,3 +57,12 @@ export const apiMiddleware = store => next => action => {
     }
   });
 };
+
+// TODO: fix the 'any' type and make it more specific.
+export const urlWithQuery = (reqUrl: string, query: any) => {
+  const url = new URL(reqUrl, window.location.origin);
+  Object.keys(query).forEach((key) => {
+    url.searchParams.append(key, query[key]);
+  });
+  return url.toString();
+};
