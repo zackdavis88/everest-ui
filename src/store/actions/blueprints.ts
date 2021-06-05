928645e52aea7b79487db59f0bb86375dd930692
Zack Davis <zack.davis88@hotmail.com>
2021-06-05 08:17:07 -0700

    add getBlueprints redux action for clientside rendering data

--- src/store/actions/blueprints.ts
+++ src/store/actions/blueprints.ts
@@ -1,6 +1,12 @@
+import { urlWithQuery } from "../utils";
 export const BLUEPRINT_REQUEST = "BLUEPRINT_REQUEST";
 export const BLUEPRINT_FAILURE = "BLUEPRINT_FAILURE";
 export const BLUEPRINT_SUCCESS = "BLUEPRINT_SUCCESS";
 export const RESET_BLUEPRINTS = "RESET_BLUEPRINTS";
 
 export const resetBlueprints = () => dispatch => dispatch({ type: RESET_BLUEPRINTS });
+
+export const getBlueprints = (query) => dispatch => dispatch({
+  types: [BLUEPRINT_REQUEST, BLUEPRINT_SUCCESS, BLUEPRINT_FAILURE],
+  reqUrl: urlWithQuery("/api/blueprints", query)
+});
