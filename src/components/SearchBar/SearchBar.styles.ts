09cf8227db66092539afe548816149bf78cf26b2
Zack Davis <zack.davis88@hotmail.com>
2021-06-07 18:24:48 -0700

    adding SearchBar component for filtering table results

---
+++ src/components/SearchBar/SearchBar.styles.ts
@@ -0,0 +1,13 @@
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import { Theme } from "@material-ui/core/styles"
+
+export const useStyles = makeStyles((theme: Theme) => ({
+  searchContainer: {
+    margin: "16px 0 0 0",
+    "& button": {
+      height: "40px",
+      margin: "0 0 0 8px",
+      padding: "3px 9px"
+    }
+  }
+}));
